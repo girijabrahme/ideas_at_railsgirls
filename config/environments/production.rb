65da138c7080d3385e1220b39b43a56e6821362b
Girija Brahme <girija@vertisinfotech.com>
2013-08-25 21:08:30 +0530

    added default url options for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Railsgirls::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'http://blooming-chamber-7659.herokuapp.com/' }
 end
